6e7921040290f539bc9859912e29c6b99ba1b259
A3AJAGBE <ajagbeaminat95@gmail.com>
2021-02-26 09:36:14 +0000

    Send an email if statement is true

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+import os
+import smtplib
+
 import requests
 from bs4 import BeautifulSoup
+from dotenv import load_dotenv
+load_dotenv()
 
 PRODUCT_URL = "https://www.amazon.com/HP-14-inch-Chromebook-Touchscreen-Bluetooth/dp/B07L52KX7B/ref=sr_1_21?dchild=1" \
               "&keywords=student+laptop&qid=1614330419&s=pc&sr=1-21 "
@@ -16,6 +21,30 @@ data = response.text
 
 # Scrape the web for the price
 soup = BeautifulSoup(data, "lxml")
-product_price = soup.find(name="span", class_="a-size-medium a-color-price priceBlockBuyingPriceString")
-price = product_price.getText()
-print(price)
+product_price = soup.find(name="span", class_="a-size-medium a-color-price priceBlockBuyingPriceString").getText()
+product_title = soup.find(id="productTitle").getText()
+price = float(product_price.split('$')[1])
+title = product_title.strip()
+
+# The amount you want to pay for the product
+TARGET_PRICE = 250
+
+# Email config
+EMAIL = os.environ.get('GMAIL')
+PASSWORD = os.environ.get('GMAIL_PASS')
+RECEIVER_EMAIL = os.environ.get('YMAIL')
+SMTP = os.environ.get('SMTP_ADDRESS')
+
+# Send an email if the product price is low or equal to the target price
+if price <= TARGET_PRICE:
+    message = f"{title} is now {price}"
+
+    with smtplib.SMTP(SMTP, port=587) as conn:
+        conn.starttls()
+        conn.login(user=EMAIL, password=PASSWORD)
+        conn.sendmail(
+            from_addr=EMAIL,
+            to_addrs=RECEIVER_EMAIL,
+            msg=f"Subject:Chrome Laptop Price Alert\n\n{message}\n{PRODUCT_URL}"
+        )
+
